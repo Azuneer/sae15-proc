2e5f53be14f53e6a4314d1b6ad0b0cb509c59268
Ewen Gadonnaud <63179515+Azuneer@users.noreply.github.com>
2023-12-09 16:03:07 +0100

    test du module de récupération de l'architecture et de la version linux

--- tests/acces_systeme.py
+++ tests/acces_systeme.py
@@ -2,14 +2,14 @@ import re
 
 def extraire_info_linux():
     try:
-        with open('/proc/net/dev', 'r') as f:
+        with open('/proc/version', 'r') as f:
             version_info = f.read()
 
             version_recherche = re.compile(r'Linux version ([^\s]+) .*?\((.*?)\)')
             correspondance = version_recherche.search(version_info)
 
             if correspondance:
-              # Extraire la version et l'architecture à partir du match
+              # Extraire la version et l'architecture à de la correspondance effectuée
               linux_version = correspondance.group(1)
               architecture = correspondance.group(2)
               return f"Version Linux: {linux_version}, Architecture: {architecture}"

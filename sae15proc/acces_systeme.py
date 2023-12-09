09c89e41aeb7ff07e7a430fb3ce71469851bfe50
Ewen Gadonnaud <63179515+Azuneer@users.noreply.github.com>
2023-12-09 16:12:42 +0100

    finalisation du module d'accès systeme #6

--- sae15proc/acces_systeme.py
+++ sae15proc/acces_systeme.py
@@ -1,6 +1,28 @@
+"""
+.. module:: acces_systeme
+  :platform: Unix, windows
+  :synopsis: module de lecture d'informations du système, nottamment la version du kernel Linux et son architecture (/proc/version)
+
+.. moduleauthor:: GADONNAUD Ewen <ewen.gadonnaud@etu.univ-poitiers.fr> & BRUNEAU Théo <theo.bruneau@etu.univ-poitiers.fr>
+"""
+
 import re
 
 def extraire_info_linux():
+    """ Fonction qui permets d'extraire la version du kernel Linux et son architecture
+    
+    :param chemin_fichier: chemin du fichier /proc/version
+    :type chemin_fichier: str
+    :returns: la version du kernel Linux et son architecture.
+    :rtype: str
+    :raises: TypeError
+    :example:
+    
+    .. code-block:: python
+     
+     systeme_version = extraire_info_linux("/proc/version")
+
+    """
     try:
         with open('/proc/version', 'r') as f:
             version_info = f.read()
